277f449cb0ddfb1bce70d30b6f6a9a146af5e5dd
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-02-04 15:02:21 -0500

    save something

--- crypt_newline.sh
+++ crypt_newline.sh
@@ -1,6 +1,15 @@
 :<<">>:"
-crypt.newline.sh
+crypt_newline.sh
 @param 1 Crypttab file.
 @param 2 Cryptfold.
-@return The key that was generated.
+@param 3 Name for the new crypt line.
+@param 4 Key for the crypttab.
+@return The key.
 >>:
+
+hash="$(echo -n "$3" | sha1sum)"
+touch "$2/$hash\&"
+mkdir "$2/$hash\|"
+target="$(mktemp)"
+
+openssl enc -d -aes-256-cbc -in "$1" -out "$target" -pass stdin <<<"$4"
